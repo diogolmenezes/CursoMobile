61ad8425b53d7013fc1ce577ebd1aa8af9116374
diogolmenezes <diogolmenezes@gmail.com>
2015-03-18 17:01:43 -0300

    incluindo localdb

--- ToDoList - Modulo 1/js/app.js
+++ ToDoList - Modulo 1/js/app.js
@@ -2,11 +2,37 @@
 
   var app = angular.module('ToDoList', []);
 
-  app.controller('ToDoListController', function(){
-    this.tasks = [];
-    this.Incluir = function(){
-      this.tasks.push({ Text: this.task_name, Completed: false });
+  app.controller('ToDoListController', function(ToDoService){
+    this.tasks = ToDoService.list();
+
+    this.add = function(){
+      this.tasks.push({ text: this.task_name, completed: false });
+      this.task_name = '';
+
+      txtTaskName.focus();
+
+      this.save();
     };
+
+    this.save = function() {
+      ToDoService.save(this.tasks);
+    };
+
+  });
+
+  app.factory('ToDoService', function() {
+      var service = {};
+
+      service.list = function() {
+        var tasks = JSON.parse(localStorage.getItem('Tasks'));
+        return  tasks ? tasks : [];
+      };
+
+      service.save = function(tasks) {
+        localStorage.setItem('Tasks', JSON.stringify(tasks));
+      };
+
+      return service;
   });
 
 })();
